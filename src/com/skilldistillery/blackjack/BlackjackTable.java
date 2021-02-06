d5798af2db76c3a7fd93cd2779bd0ff6bf785e97
Nick Bird <bird.nick.95@gmail.com>
2021-02-06 15:27:40 -0700

    additional fixes to game logic

--- src/com/skilldistillery/blackjack/BlackjackTable.java
+++ src/com/skilldistillery/blackjack/BlackjackTable.java
@@ -35,12 +35,17 @@ public void checkForPlayerBust(BlackjackHand hand) {
 	if (hand.isBust()) {
 		System.out.println("You have gone bust");
 		roundGoing = false;
+		keepHitting = false;
+		
 	}
 }
-	public void checkForBlackjack(BlackjackHand hand) {
+	public boolean checkForBlackjack(BlackjackHand hand) {
 		if (hand.isBlackjack()) {
 			System.out.println("Blackjack!");
-			roundGoing = false;
+			return true;
+		}
+		else {
+			return false;
 		}
 }
 	public void checkForDealerBust(BlackjackHand hand) {
@@ -49,6 +54,12 @@ public void checkForPlayerBust(BlackjackHand hand) {
 			roundGoing = false;
 		}
 }
+	public void checkForDealerBlackJack(BlackjackHand hand) {
+		if (hand.isBlackjack()) {
+			System.out.println("The dealer has Blackjack");
+			roundGoing = false;
+		}
+	}
 public void checkForWin(BlackjackHand hand1, BlackjackHand hand2) {
 	if (hand1.getHandValue() > hand2.getHandValue() && hand1.getHandValue() < 22) {
 		System.out.println("You have won");
@@ -61,18 +72,23 @@ public void checkForWin(BlackjackHand hand1, BlackjackHand hand2) {
 }
 	
 public void playRound() {
-	System.out.println("Welcome to Blackjack: ");
+	
+	while(roundGoing) {
+	System.out.println("Welcome to Blackjack! ");
 	player.addCard(dealer.dealCard());
-	System.out.println("You have drawn a: ");
+	System.out.println("You have drawn: ");
 	player.printHand();
 	
 	dealer.addCard(dealer.dealCard());
-	System.out.println("The dealer has drawn a: ");
+	System.out.println("The dealer has drawn: ");
 	dealer.printDealerHand();
 	
 	player.addCard(dealer.dealCard());
-	System.out.println("You have drawn a: ");
+	System.out.println("You have drawn: ");
 	player.printHand();
+	if(checkForBlackjack(player.getHand())) {
+		break;
+	}
 	dealer.addCard(dealer.dealCard());
 	System.out.println("The dealer draws a card and places it face down.");
 	
@@ -80,9 +96,9 @@ public void playRound() {
 //	dealer.printDealerHand();
 
 //	checkForBustOrBlackjack(player.getHand(), dealer.getHand());
-	checkForBlackjack(player.getHand());
+	
 
-	while(roundGoing) {
+	
 		while(keepHitting) {
 		System.out.println("Enter 1 to hit or 0 to stay: ");
 		int i = sc.nextInt();
@@ -98,9 +114,10 @@ public void playRound() {
 			keepHitting = false;
 		}
 		}
-	
+	if(roundGoing) {
 		System.out.println("The dealer flips the facedown card to reveal the hand: ");
 		dealer.printDealerHand();
+		checkForDealerBlackJack(dealer.getHand());
 
 		while(dealer.dealerHit(dealer.getHand())) {
 		System.out.println("The dealer draws a: ");
@@ -109,12 +126,14 @@ public void playRound() {
 		System.out.println(newDealerCard);
 		
 		checkForDealerBust(dealer.getHand());
+		}
 	}
 	checkForWin(player.getHand(), dealer.getHand());
 }
 	
 	
 }
-	
-	
 }
+	
+	
+
